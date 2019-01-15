c58a94218de107bb58d04a788545ed58664a892c
cswuyp <cswuyepeng@gmail.com>
2019-01-15 10:47:17 +0800

    update

---
+++ net2/level.js
@@ -0,0 +1,14 @@
+'use strict';
+// 导入MySql模块
+var mysql = require('mysql');
+const { query } = require('../sql/DBConfig');
+var userSQL = require('../sql/Usersql');
+var LevelStar=require('./levelStar');
+
+var Account=15622184887;
+var LevelId=1;
+async function test(){
+	LevelStar.selectLevelStarSum(Account,LevelId);
+	LevelStar.levelStar(Account);
+}
+test();
